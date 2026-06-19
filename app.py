ec1aa5e7066d310d80fbc36deb1c068aaee4cf2c
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-06-19 21:25:55 +0530

    Add Flask app for face detection using OpenCV

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier(
+    "haarcascade_frontalface_default.xml"
+)
+
+def generate_frames():
+
+    while True:
+
+        success, frame = camera.read()
+
+        if not success:
+            break
+
+        gray = cv2.cvtColor(
+            frame,
+            cv2.COLOR_BGR2GRAY
+        )
+
+        faces = face_cascade.detectMultiScale(
+            gray,
+            1.3,
+            5
+        )
+
+        for (x, y, w, h) in faces:
+
+            cv2.rectangle(
+                frame,
+                (x, y),
+                (x+w, y+h),
+                (0,255,0),
+                2
+            )
+
+            cv2.putText(
+                frame,
+                "Driver Detected",
+                (x, y-10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.7,
+                (0,255,0),
+                2
+            )
+
+        ret, buffer = cv2.imencode(
+            '.jpg',
+            frame
+        )
+
+        frame = buffer.tobytes()
+
+        yield (
+            b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n'
+            + frame +
+            b'\r\n'
+        )
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+
+    return Response(
+        generate_frames(),
+        mimetype=
+        'multipart/x-mixed-replace; boundary=frame'
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
