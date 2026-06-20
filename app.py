10430ea1c33f38c08dda6e01956280c295d95b64
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-06-20 17:16:50 +0530

    Add /detect_objects route to return JSON data

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+    @app.route("/detect_objects", methods=["POST"])
+def detect_objects():
+
+    return jsonify({
+        "phone": False,
+        "seatbelt": True,
+        "persons": 1
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
