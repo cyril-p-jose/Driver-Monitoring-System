93b5dffb0075888f682f253f54db0c471d06ec64
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-06-20 09:30:39 +0530

    Refactor drowsiness detection logic and add audio alarm

--- static/script.js
+++ static/script.js
@@ -1,5 +1,3 @@
-const alarm = new Audio("/static/alarm.mp3");
-
 const video = document.getElementById("video");
 
 const drowsiness =
@@ -7,6 +5,8 @@ document.getElementById("drowsiness");
 
 const faceStatus =
 document.getElementById("faceStatus");
+
+const alarm = new Audio("/static/alarm.mp3");
 
 navigator.mediaDevices
 .getUserMedia({ video: true })
@@ -27,20 +27,75 @@ faceMesh.setOptions({
     minTrackingConfidence: 0.5
 });
 
-let lastFaceTime = Date.now();
+let eyesClosedStart = null;
+
+// Distance between two landmarks
+function distance(p1, p2){
+
+    const dx = p1.x - p2.x;
+    const dy = p1.y - p2.y;
+
+    return Math.sqrt(dx * dx + dy * dy);
+}
 
 faceMesh.onResults(results => {
 
-    if(results.multiFaceLandmarks &&
-       results.multiFaceLandmarks.length > 0){
+    if(
+        results.multiFaceLandmarks &&
+        results.multiFaceLandmarks.length > 0
+    ){
 
         faceStatus.innerHTML =
         "Face Detected";
 
-        drowsiness.innerHTML =
-        "Normal";
+        const face =
+        results.multiFaceLandmarks[0];
 
-        lastFaceTime = Date.now();
+        // Left Eye Landmarks
+        const leftTop = face[159];
+        const leftBottom = face[145];
+        const leftLeft = face[33];
+        const leftRight = face[133];
+
+        const eyeHeight =
+        distance(leftTop, leftBottom);
+
+        const eyeWidth =
+        distance(leftLeft, leftRight);
+
+        const EAR =
+        eyeHeight / eyeWidth;
+
+        console.log("EAR:", EAR);
+
+        if(EAR < 0.15){
+
+            if(!eyesClosedStart){
+
+                eyesClosedStart =
+                Date.now();
+
+            }
+
+            const closedTime =
+            Date.now() - eyesClosedStart;
+
+            if(closedTime > 2000){
+
+                drowsiness.innerHTML =
+                "🚨 DROWSINESS DETECTED";
+
+                alarm.play();
+            }
+
+        }
+        else{
+
+            eyesClosedStart = null;
+
+            drowsiness.innerHTML =
+            "Normal";
+        }
 
     }
     else{
@@ -48,18 +103,15 @@ faceMesh.onResults(results => {
         faceStatus.innerHTML =
         "No Face";
 
-        if(Date.now() - lastFaceTime > 3000){
-
-            drowsiness.innerHTML =
-            "ALERT";
-                alarm.play();
-        }
-
+        drowsiness.innerHTML =
+        "No Face";
     }
 
 });
 
-video.addEventListener("loadeddata", async () => {
+video.addEventListener(
+"loadeddata",
+async () => {
 
     async function detect(){
 
@@ -71,5 +123,4 @@ video.addEventListener("loadeddata", async () => {
     }
 
     detect();
-
 });
